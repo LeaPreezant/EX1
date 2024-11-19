3da19c7fd82e36cc76efa55ebcb45da0ecee190a
LeaPreezant <preezlea@gmail.com>
2024-11-19 23:02:30 +0200

    Ex1 Part1 Tests Passedgit status!

--- lesson_1/funcQueue.cpp
+++ lesson_1/funcQueue.cpp
@@ -1,21 +1,68 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "Queue.h"
+#include <iostream>
+#include <sstream>
+#include <windows.h>
 
 void initQueue(Queue* q, unsigned int size)
 {
-	q->data = (int*)malloc(size * sizeof(int));
-    q->first = 0; // empty
-    q->last = 0;
-    q->maxSize = size;
-    q->size = 0;
+    q->_elements = new int[size];
+    q->_maxSize = size;
+    q->_count = 0;
 }
 
 void cleanQueue(Queue* q)
 {
-    if (q->data != NULL) 
+    if (q->_elements != NULL)
     {
-        free(q->data);  // free after malloc
-        q->data = NULL; 
+        delete[] q->_elements;  // free 
+        q->_elements = NULL;
     }
+    // reset 
+    q->_maxSize = 0;
+    q->_count = 0;
+}
+
+void enqueue(Queue* q, unsigned int newValue)
+{
+    if (q->_count < q->_maxSize)
+    {
+        int i = 0;
+        while (i < q->_count)
+        {
+            i++;
+        }
+        q->_elements[i] = newValue;
+        q->_count++;
+    }
+}
+
+int dequeue(Queue* q) // return element in top of queue, or -1 if empty
+{
+    if (q->_count == 0)
+    {
+        return -1;
+    }
+    int firstElement = q->_elements[0];
+    int i = 0;
+    for (i = 0; i < q->_count - 1; i++) 
+    {
+        q->_elements[i] = q->_elements[i + 1];
+    }
+    q->_elements[i] = 0;
+    q->_count--;
+    return firstElement;
+}
+
+
+bool isEmpty(Queue* s)
+{
+    if (s->_count == 0)
+        return true;
+    return false;
+}
+bool isFull(Queue* s)
+{
+    if (s->_count == 0)
+        return false;
+    return true;
 }
